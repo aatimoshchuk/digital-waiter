9fdc4f6d7d2d19490102df843cbde1f0a58ca5fd
a.timoshchuk <a.timoshchuk@g.nsu.ru>
2025-05-26 20:21:08 +0700

    Настроен swagger [2]

--- digital-waiter-backend/src/main/java/nsu/sber/config/security/OpenApiConfig.java
+++ digital-waiter-backend/src/main/java/nsu/sber/config/security/OpenApiConfig.java
@@ -5,17 +5,33 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${digital-waiter.url}")
+    private String digitalWaiterUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Digital Waiter API")
                         .description("Документация для Цифрового Официанта"))
+                .servers(List.of(
+                        new Server()
+                                .url(digitalWaiterUrl)
+                                .description("Production server"),
+                        new Server()
+                                .url("http://localhost")
+                                .description("Local Dev")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
