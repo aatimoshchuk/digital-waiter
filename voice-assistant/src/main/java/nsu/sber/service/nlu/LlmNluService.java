730b9f10d9f3fb3dcfa738c4fc16df8797224732
ataskaev <ataskaev@yandex.ru>
2025-11-08 15:34:04 +0700

    create nlu assingment

--- voice-assistant/src/main/java/nsu/sber/service/nlu/LlmNluService.java
+++ voice-assistant/src/main/java/nsu/sber/service/nlu/LlmNluService.java
@@ -1,4 +1,11 @@
 package nsu.sber.service.nlu;
 
-public class LlmNluService {
+public class LlmNluService implements NluService {
+
+    @Override
+    public String parse(String text, String context) {
+        return "The type that can be parsed into the command";
+    }
+
+
 }
--- voice-assistant/src/main/java/nsu/sber/service/nlu/NluService.java
+++ voice-assistant/src/main/java/nsu/sber/service/nlu/NluService.java
@@ -1,4 +1,8 @@
 package nsu.sber.service.nlu;
 
 public interface NluService {
+
+    // TODO: подумать над возвращаемым типом
+    String parse(String text, String context);
+
 }
