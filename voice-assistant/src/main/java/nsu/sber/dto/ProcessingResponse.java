456b17b80d4a907bd1b611178cfde5fe67dd5a8f
ataskaev <ataskaev@yandex.ru>
2025-11-08 18:36:03 +0700

    update response dto

--- voice-assistant/src/main/java/nsu/sber/dto/ProcessingResponse.java
+++ voice-assistant/src/main/java/nsu/sber/dto/ProcessingResponse.java
@@ -1,4 +1,16 @@
 package nsu.sber.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProcessingResponse {
+    private boolean success;
+    private String transcribedText;
+
 }
