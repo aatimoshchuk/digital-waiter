109d8b00970f651b9b3f867f07d85fd02f7f0aaa
a.timoshchuk <a.timoshchuk@g.nsu.ru>
2025-12-02 00:12:54 +0700

    Аннотации для сваггера

--- digital-waiter-backend/src/main/java/nsu/sber/web/dto/SignInRequestDto.java
+++ digital-waiter-backend/src/main/java/nsu/sber/web/dto/SignInRequestDto.java
@@ -1,5 +1,6 @@
 package nsu.sber.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -7,8 +8,16 @@ import lombok.Data;
 public class SignInRequestDto {
 
     @NotBlank
+    @Schema(
+            description = "Login for authorization in the guest or administrative interface",
+            example = "sparks_1"
+    )
     private String login;
 
     @NotBlank
+    @Schema(
+            description = "Password associated with the login",
+            example = "Qwerty123"
+    )
     private String password;
 }
