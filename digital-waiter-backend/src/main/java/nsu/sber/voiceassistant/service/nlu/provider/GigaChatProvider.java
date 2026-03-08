ae5e4449cda899d332b50bc115da1aa7d14da499
a.timoshchuk <a.timoshchuk@g.nsu.ru>
2026-03-08 13:25:30 +0700

    Fix imports

--- digital-waiter-backend/src/main/java/nsu/sber/voiceassistant/service/nlu/provider/GigaChatProvider.java
+++ digital-waiter-backend/src/main/java/nsu/sber/voiceassistant/service/nlu/provider/GigaChatProvider.java
@@ -16,7 +16,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 import javax.net.ssl.SSLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Component
